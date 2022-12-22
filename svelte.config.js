87525c9bb2af5d768c3e24b6838cd5b459333a9a
Sandeep Yadav <sandeep.yadav.consultant@heart.org>
2022-12-22 19:04:20 +0530

    second commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,9 @@
-// We have changed the adapter line to use adapter-node@next
-import adapter from '@sveltejs/adapter-node';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
 	kit: {
-		// We have changed this to point to a build directory
-		adapter: adapter({ out: 'build' })
+		adapter: adapter()
 	}
 };
 
